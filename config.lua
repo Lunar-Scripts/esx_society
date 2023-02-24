7779b2c86fdabd22e4836d1a504e9332978bdeb4
Lunar-Scripts <119682264+Lunar-Scripts@users.noreply.github.com>
2023-02-24 18:18:32 +0100

    Add Config.BossGrades

--- config.lua
+++ config.lua
@@ -3,3 +3,9 @@ Config = {}
 Config.Locale = GetConvar('esx:locale', 'en')
 Config.EnableESXIdentity = true
 Config.MaxSalary = 3500
+Config.BossGrades = {
+  'boss',
+  'staff1',
+  'staff2',
+  'staff3'
+}
