88745a1dae34e90dc866524130ec0a455b60dcac
Lunar-Scripts <119682264+Lunar-Scripts@users.noreply.github.com>
2023-02-24 18:19:07 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,8 +4,8 @@ Config.Locale = GetConvar('esx:locale', 'en')
 Config.EnableESXIdentity = true
 Config.MaxSalary = 3500
 Config.BossGrades = {
-  'boss',
-  'staff1',
-  'staff2',
-  'staff3'
+  ['boss'] = true,
+  ['staff1'] = true,
+  ['staff2'] = true,
+  ['staff3'] = true
 }
